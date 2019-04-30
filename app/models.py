9889892d597d243d76bf07d1edd1e4c7dfc5875e
ArturCRS <43724262+ArturCRS@users.noreply.github.com>
2019-04-30 16:18:11 -0300

    Estrutura inicial do modelo

--- app/models.py
+++ app/models.py
@@ -3,19 +3,82 @@ from datetime import date
 from app import db
 class Event(db.Model): 
     """
-    Create an Event table
+    Create a Event table
     """
     __tablename__ = 'events'
     
     id = db.Column(db.Integer, primary_key=True)
-    name_event = db.Column(db.String(256))
-    event_location = db.Column(db.String(512))
+    name = db.Column(db.String(256))
+    location = db.Column(db.String(512))
     coverage_area = db.Column(db.String(64))
-    date_start_event = db.Column(db.Date)
-    date_end_event = db.Column(db.Date)
-    event_description = db.Column(db.String(2048))
-    event_coordinator = db.Column(db.Integer, nullable=False) 
-    #event_coordinator = models.OneToOneField(User, on_delete=models.CASCADE) << Criar chave Foreign key 
+    date_start = db.Column(db.Date)
+    date_end = db.Column(db.Date)
+    description = db.Column(db.String(2048))
+    coordinator = db.Column(db.Integer, nullable=False) 
+    trail = db.relationship('Trail', backref='events', cascade='all, delete-orphan', lazy='dynamic')
+    
+    def __repr__(self):
+        return '<>Evento: {}'.format(self.name)
+
+class Trail(db.Model):
+    """
+    Create a Trail table
+    """
+    __tablename__ = 'trail'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    date_start = db.Column(db.Date)
+    date_end = db.Column(db.date)
+    coordinator = db.Column(db.Integer)
+    belong = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
+    checklist = db.relationship('Checklist', backref='trail', cascade='all, delete-orphan', lazy='dynamic')
+    appraiser = db.relationship('Appraiser', backref='trail', cascade='all, delete-orphan', lazy='dynamic')
+     
+    def __repr__(self):
+        return '<>Trilha: {}'.format(self.name)
+
+class Submission(db.Model):
+    """
+    Create a Submission table
+    """
+    __tablename__ = 'submission'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    description = db.Column(db.String(2048))
+    file = db.Column(db.String(256))
+    status = db.Column(db.String(64))
+    grade = db.Column(db.Integer)
+    author = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<>Evento: {}'.format(self.name_event)
+        return '<>Submissao: {}'.format(self.title)
+
+class Checklist(db.Model):
+    """
+    Create a Checklist table
+    """
+    __tablename__ = 'checklist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256))
+    weight = db.Column(db.Integer)
+    belong = db.Column(db.Integer, db.ForeignKey('trail.id'), nullable=False)
+
+    def __repr__(self):
+        return '<>Checklist: {}'.format(self.description)
+
+class Appraiser(db.Model):
+    """
+    Create a Appraiser table
+    """
+    __tablename__ = 'appraiser'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer)
+    in_trail = db.Column(db.Integer, db.ForeignKey('trail.id'), nullable=False)
+    appraise = db.relationship('Trail')
+
+    #def __repr__(self):
+    #   return '<>Checklist: {}'.format(self.description)
